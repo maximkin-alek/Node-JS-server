392210c19378e4b014935d887ddc627739a4890e
Alexander Maximkin <maximkin.alek@yandex.ru>
2020-09-07 23:15:35 +0300

    install Joi

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,21 @@
 const cardsRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+Joi.objectId = require('joi-objectid')(Joi);
 
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 cardsRouter.get('/', getCards);
-cardsRouter.post('/', createCard);
+cardsRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().min(2),
+    owner: Joi.objectId(),
+    likes: Joi.string().required().min(2),
+    createdAt: Joi.string().required().min(2),
+  }),
+}), createCard);
 cardsRouter.delete('/:id', deleteCard);
 cardsRouter.put('/:id/likes', likeCard);
 cardsRouter.delete('/:id/likes', dislikeCard);
